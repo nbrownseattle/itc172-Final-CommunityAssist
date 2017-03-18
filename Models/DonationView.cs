91bfd6b77704a3dea326d299af8adfe2b489bc16
nbrownseattle <nbrown28@seattlecentral.edu>
2017-03-17 17:52:04 -0700

    Adds Controller and Models files
    Adds files for final project

---
+++ Models/DonationView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.Models;
+
+namespace FinalProject.Models
+{
+    public class DonationView
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public decimal DonationAmount { set; get; }
+        public DateTime DonationDate { set; get; }
+    }
+}
